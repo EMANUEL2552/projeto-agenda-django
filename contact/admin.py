3c4763cabdd230bde943ba4cc167569b40ef410a
Emanuel <emanuel.fernandesdasilva@gmail.com>
2023-05-08 13:20:04 -0300

    ajuste no model

--- contact/admin.py
+++ contact/admin.py
@@ -5,4 +5,10 @@ from contact import models  #importaçao do model da classe contact
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    list_display = 'id', 'first_name', 'last_name', 'phone',
+    ordering = 'id', # escolha do campo para ordenação
+    search_fields = 'id', 'first_name', 'last_name',  # adiciona um campo de pesquisa com esses atributos
+    list_per_page = 1  # adiciona paginacao
+    list_max_show_all = 200  # quantidade maxima que vc quer por pagina
+    list_editable = 'first_name', 'last_name',  # campos para ediçao diretamente na pagina
+    list_display_links = 'phone', 'id',  # campos que pode ser links
