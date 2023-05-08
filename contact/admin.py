c5df2539af64fbadf072bede4275310b1d7fbd87
Emanuel <emanuel.fernandesdasilva@gmail.com>
2023-05-08 17:20:32 -0300

    mais ajustes no model na url e no settings

--- contact/admin.py
+++ contact/admin.py
@@ -8,7 +8,7 @@ class ContactAdmin(admin.ModelAdmin):
     list_display = 'id', 'first_name', 'last_name', 'phone',
     ordering = 'id', # escolha do campo para ordenação
     search_fields = 'id', 'first_name', 'last_name',  # adiciona um campo de pesquisa com esses atributos
-    list_per_page = 1  # adiciona paginacao
+    list_per_page = 5  # adiciona paginacao
     list_max_show_all = 200  # quantidade maxima que vc quer por pagina
     list_editable = 'first_name', 'last_name',  # campos para ediçao diretamente na pagina
     list_display_links = 'phone', 'id',  # campos que pode ser links
