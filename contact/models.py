b18a131271e71d65ff3311484d0f7803513dce1a
Emanuel <emanuel.fernandesdasilva@gmail.com>
2023-05-09 13:41:30 -0300

    criacao do campo owner

--- contact/models.py
+++ contact/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        
     name = models.CharField(max_length=50)
     def __str__(self) -> str:
         return f'{self.name}'
@@ -25,5 +30,9 @@ class Contact(models.Model):
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
 
-    
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+    )
     
