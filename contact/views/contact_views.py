b3d278366715203f51fc88f62c8800965feeeb1e
Emanuel <emanuel.fernandesdasilva@gmail.com>
2023-05-12 18:20:34 -0300

    criacao da paginacao

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -2,16 +2,19 @@ from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
 from django.http import Http404
 from django.db.models import Q
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 def index(request):
     contacts = Contact.objects  \
           .filter(show=True)\
-          .order_by('-id')[0:10]  # para mostrar somente 10 valores na pag
-    
+          .order_by('-id')    
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - '
     }
     
@@ -39,11 +42,16 @@ def search(request):
                 
                 )\
         .order_by('-id')  
-    print(contacts.query)    
+    print(contacts.query)  
+    
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
         
     context = {
-        'contacts': contacts,
-        'site_title': 'Search - '
+        'page_obj': page_obj,
+        'site_title': 'Search - ',
+        'search_value': search_value,
     }  
     
     return render(
